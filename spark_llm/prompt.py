ed1cc113af9967a6f504292c4b49ea22dcf511dd
Amanda Liu <amanda.liu@databricks.com>
2023-06-15 10:48:40 -0700

    Update examples

--- spark_llm/prompt.py
+++ spark_llm/prompt.py
@@ -121,10 +121,10 @@ There is no need to install any package with pip.
 """
 
 VERIFY_TEMPLATE = """
-Given 1) a PySpark dataframe, my_df, and 2) a description of expected properties, desc,
+Given 1) a PySpark dataframe, df, and 2) a description of expected properties, desc,
 generate a Python function to test whether the given dataframe satisfies the expected properties.
 Your generated function should take 1 parameter, df, and the return type should be a boolean.
-You will call the function, passing in my_df as the parameter, and return the output (True/False).
+You will call the function, passing in df as the parameter, and return the output (True/False).
 
 In total, your output must follow the format below, exactly (no explanation words):
 1. function definition f, in Python
@@ -133,7 +133,7 @@ In total, your output must follow the format below, exactly (no explanation word
 
 For example:
 Input:
-my_df = spark.createDataFrame(data=[("Alice", 25), ("Bob", 30), ("Charlie", 35)], schema=["Name", "Age"])
+df = DataFrame[name: string, age: int]
 desc = "expect 5 columns"
 
 Output:
@@ -146,14 +146,13 @@ Output:
         return True
     else:
         return False
-        
+
 FUNCTION_NAME: has_5_columns"
 
-Here is your input df: {my_df}
+Here is your input df: {df}
 Here is your input description: {desc}
 """
 
-
 VERIFY_PROMPT = PromptTemplate(
-    input_variables=["my_df", "desc"], template=VERIFY_TEMPLATE
+    input_variables=["df", "desc"], template=VERIFY_TEMPLATE
 )
