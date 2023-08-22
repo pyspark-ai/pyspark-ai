c4e0952917446147071a7ed7a81214ce922dd333
Gengliang Wang <gengliang@apache.org>
2023-08-22 10:27:17 -0700

    able to generate queries

--- benchmark/wiki_sql.py
+++ benchmark/wiki_sql.py
@@ -3,11 +3,13 @@ from argparse import ArgumentParser
 
 from pyspark.sql import SparkSession
 
+from pyspark_ai import SparkAI
 
-def generate_sql_statements(data_file):
+
+def generate_sql_statements(table_file):
     sql_statements = []
 
-    with open(data_file, 'r') as f:
+    with open(table_file, 'r') as f:
         for line in f:
             item = json.loads(line.strip())
 
@@ -29,16 +31,38 @@ def generate_sql_statements(data_file):
 
     return sql_statements
 
+
+# Parse questions and tables from the source file
+def get_tables_and_questions(source_file):
+    tables = []
+    questions = []
+    with open(source_file, 'r') as f:
+        for line in f:
+            item = json.loads(line.strip())
+            tables.append(item['table_id'])
+            questions.append(item['question'])
+    return tables, questions
+
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('table_file', help='table definition file')
-    # parser.add_argument('source_file', help='source file for the prediction')
+    parser.add_argument('source_file', help='source file for the prediction')
     args = parser.parse_args()
 
     # Example usage:
-    data_file = args.table_file
-    statements = generate_sql_statements(data_file)
+    table_file = args.table_file
+    statements = generate_sql_statements(table_file)
     spark = SparkSession.builder.getOrCreate()
     for stmt in statements:
-        print(stmt)
         spark.sql(stmt)
+
+    source_file = args.source_file
+    tables, questions = get_tables_and_questions(source_file)
+    spark_ai = SparkAI(spark_session=spark)
+    # Create sql query for each question and table
+    for table, question in zip(tables, questions):
+        df = spark.table(f"`{table}`")
+        sql_query = spark_ai._get_transform_sql_query(df, question, cache=False)
+        print(sql_query)
+
