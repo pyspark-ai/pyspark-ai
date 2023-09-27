047cfc01f0385b3f46c2136c33f09d6a1a9dbe66
Amanda Liu <amanda.liu@databricks.com>
2023-09-27 15:30:38 -0400

    use llm mock in tests

--- tests/test_ai_tools.py
+++ tests/test_ai_tools.py
@@ -1,13 +1,21 @@
 import unittest
+from unittest.mock import MagicMock
+from langchain.base_language import BaseLanguageModel
+from pyspark.sql import SparkSession
 
 from pyspark_ai.pyspark_ai import SparkAI
 from pyspark_ai.tool import QuerySparkSQLTool, QueryValidationTool, SimilarValueTool
 
 
 class TestToolsInit(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.spark = SparkSession.builder.getOrCreate()
+        cls.llm_mock = MagicMock(spec=BaseLanguageModel)
+
     def test_exclude_similar_value_tool(self):
         """Test that SimilarValueTool is excluded by default"""
-        spark_ai = SparkAI()
+        spark_ai = SparkAI(llm=self.llm_mock, spark_session=self.spark)
         agent = spark_ai._create_sql_agent()
         self.assertEqual(
             agent.tools,
@@ -20,7 +28,11 @@ class TestToolsInit(unittest.TestCase):
     def test_include_similar_value_tool(self):
         """Test that SimilarValueTool is included when vector_store_dir is specified"""
         vector_store_dir = "temp/"
-        spark_ai = SparkAI(vector_store_dir=vector_store_dir)
+        spark_ai = SparkAI(
+            llm=self.llm_mock,
+            spark_session=self.spark,
+            vector_store_dir=vector_store_dir,
+        )
         agent = spark_ai._create_sql_agent()
         self.assertEqual(
             agent.tools,
