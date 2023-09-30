dfe56d79ba5239d916337f4bec5111a592d6343c
SemyonSinchenko <semyonsinchenko@yahoo.com>
2023-09-30 10:42:49 +0000

    Add three end2end tests of UDF generation.
    1. UDF that extract an email from raw text
    2. UDF that parse heterogeneous JSON and return array of strings
    3. Math function that generate random number from Lapalce distribution
    
     Changes to be committed:
    	modified:   .gitignore
    	modified:   tests/test_spark_ai.py

--- tests/test_spark_ai.py
+++ tests/test_spark_ai.py
@@ -1,15 +1,20 @@
-import unittest
+import json
 import logging
+import unittest
 from io import StringIO
+from random import random, shuffle
+from typing import List
 from unittest.mock import MagicMock
 
+import numpy as np
 from chispa.dataframe_comparer import assert_df_equality
 from langchain.base_language import BaseLanguageModel
-from pyspark.sql import SparkSession, Row
-from tiktoken import Encoding
-
+from pyspark.sql import Row, SparkSession
+from pyspark.sql.functions import expr, lit, mean, stddev
+from pyspark.sql.types import ArrayType, StringType, DoubleType
 from pyspark_ai import SparkAI
 from pyspark_ai.search_tool_with_cache import SearchToolWithCache
+from tiktoken import Encoding
 
 
 class SparkAIInitializationTestCase(unittest.TestCase):
@@ -38,7 +43,6 @@ class SparkAIInitializationTestCase(unittest.TestCase):
 
 
 class TestGetTableCommentFromExplain(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
         cls.spark = SparkSession.builder.getOrCreate()
@@ -49,27 +53,39 @@ class TestGetTableCommentFromExplain(unittest.TestCase):
     def tearDownClass(cls):
         cls.spark.stop()
 
-    def create_and_read_table(self, table_name, data, comment=''):
+    def create_and_read_table(self, table_name, data, comment=""):
         self.spark.createDataFrame(data, ["col1", "col2"]).write.saveAsTable(table_name)
-        if comment != '':
-            self.spark.sql(f"ALTER TABLE {table_name} SET TBLPROPERTIES ('comment' = '{comment}')")
+        if comment != "":
+            self.spark.sql(
+                f"ALTER TABLE {table_name} SET TBLPROPERTIES ('comment' = '{comment}')"
+            )
         return self.spark.sql(f"SELECT * FROM {table_name}")
 
     def test_single_table(self):
         table_name = "spark_catalog.default.test_table1"
         comment = "comment1"
         try:
-            df = self.create_and_read_table(table_name, [(1, "foo"), (2, "bar")], comment)
+            df = self.create_and_read_table(
+                table_name, [(1, "foo"), (2, "bar")], comment
+            )
             tables = SparkAI._get_tables_from_explain(df)
             self.assertEqual(tables, [table_name])
-            self.assertEqual(self.spark_ai._get_table_comment(df), "which represents comment1")
+            self.assertEqual(
+                self.spark_ai._get_table_comment(df), "which represents comment1"
+            )
         finally:
             self.spark.sql(f"DROP TABLE IF EXISTS {table_name}")
 
     def test_multiple_tables(self):
-        table_names = ["spark_catalog.default.test_table1", "spark_catalog.default.test_table2"]
+        table_names = [
+            "spark_catalog.default.test_table1",
+            "spark_catalog.default.test_table2",
+        ]
         try:
-            dfs = [self.create_and_read_table(name, [(1, "foo"), (2, "bar")]) for name in table_names]
+            dfs = [
+                self.create_and_read_table(name, [(1, "foo"), (2, "bar")])
+                for name in table_names
+            ]
             df = dfs[0].join(dfs[1], "col1")
             tables = SparkAI._get_tables_from_explain(df)
             self.assertEqual(tables, table_names)
@@ -84,6 +100,7 @@ class TestGetTableCommentFromExplain(unittest.TestCase):
         tables = SparkAI._get_tables_from_explain(df)
         self.assertEqual(tables, [])
         self.assertEqual(self.spark_ai._get_table_comment(df), "")
+
 
 class SparkAITrimTextTestCase(unittest.TestCase):
     """Test cases for the _trim_text_from_end method of the SparkAI."""
@@ -200,7 +217,9 @@ class CacheRetrievalTestCase(SparkTestCase):
         self.spark_ai = SparkAI(
             llm=self.llm_mock, cache_file_location="examples/spark_ai_cache.json"
         )
-        self.url = "https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States"
+        self.url = (
+            "https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States"
+        )
         self.df1 = self.spark_ai.create_df(self.url, ["president", "vice_president"])
 
     def test_create_df(self):
@@ -256,7 +275,6 @@ class SparkAnalysisTest(SparkTestCase):
 
 
 class SparkConnectTestCase(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
         cls.spark = SparkSession.builder.remote("sc://localhost").getOrCreate()
@@ -265,32 +283,38 @@ class SparkConnectTestCase(unittest.TestCase):
     def tearDownClass(cls):
         cls.spark.stop()
 
+
 @unittest.skip("skip until GHA secret key enabled")
 class SparkConnectTests(SparkConnectTestCase):
     def setUp(self):
         self.spark_ai = SparkAI(
-            cache_file_location="examples/spark_ai_cache.json",
-            verbose=True
+            cache_file_location="examples/spark_ai_cache.json", verbose=True
         )
         self.spark_ai.activate()
 
     def test_spark_connect_autodf_e2e(self):
         try:
-            df = self.spark_ai.create_df("https://www.carpro.com/blog/full-year-2022-national-auto-sales-by-brand")
+            df = self.spark_ai.create_df(
+                "https://www.carpro.com/blog/full-year-2022-national-auto-sales-by-brand"
+            )
 
             # Test df aggregation happens before Pandas conversion
             self.root_logger = logging.getLogger()
             self.log_capture_string = StringIO()
             self.ch = logging.StreamHandler(self.log_capture_string)
             self.root_logger.addHandler(self.ch)
-            df.ai.plot("pie chart for US sales market shares, show the top 5 brands and the sum of others")
+            df.ai.plot(
+                "pie chart for US sales market shares, show the top 5 brands and the sum of others"
+            )
             log_contents = self.log_capture_string.getvalue()
             self.root_logger.removeHandler(self.ch)
             groupby_index = log_contents.find("groupBy")
             toPandas_index = log_contents.find("toPandas")
             self.assertTrue(
-                groupby_index != -1 and toPandas_index != -1 and groupby_index < toPandas_index,
-                "the aggregation 'groupby' should appear before 'toPandas'"
+                groupby_index != -1
+                and toPandas_index != -1
+                and groupby_index < toPandas_index,
+                "the aggregation 'groupby' should appear before 'toPandas'",
             )
 
             df.ai.explain()
@@ -308,15 +332,17 @@ class SparkConnectTests(SparkConnectTestCase):
                     ("mountain bike", 10),
                     ("electric bike", 5),
                     ("road bike", 3),
-                    ("cruisers bike", 8)
+                    ("cruisers bike", 8),
                 ],
-                ["product_category", "product_count"]
+                ["product_category", "product_count"],
             )
             result = df.ai.transform("list top 3 products by count")
 
-            expected_lst = [Row(product_category='children bike', product_count=20),
-                            Row(product_category='comfort bike', product_count=15),
-                            Row(product_category='mountain bike', product_count=10)]
+            expected_lst = [
+                Row(product_category="children bike", product_count=20),
+                Row(product_category="comfort bike", product_count=15),
+                Row(product_category="mountain bike", product_count=10),
+            ]
 
             self.assertEqual(result.collect(), expected_lst)
         except Exception:
@@ -334,17 +360,145 @@ class SparkConnectTests(SparkConnectTestCase):
                     ("C", "English", 90),
                     ("C", "Science", 100),
                 ],
-                ["Student", "Subject", "Marks"]
+                ["Student", "Subject", "Marks"],
             )
             result = df.ai.transform("pivot using Subject for Marks")
 
-            expected_lst = [Row(Student='B', English=75, Maths=80, Science=None),
-                            Row(Student='C', English=90, Maths=None, Science=100),
-                            Row(Student='A', English=45, Maths=50, Science=None)]
+            expected_lst = [
+                Row(Student="B", English=75, Maths=80, Science=None),
+                Row(Student="C", English=90, Maths=None, Science=100),
+                Row(Student="A", English=45, Maths=50, Science=None),
+            ]
 
             self.assertEqual(result.collect(), expected_lst)
         except Exception:
             self.fail("Spark Connect pivot error")
+
+
+class UDFGenerationTest(SparkTestCase):
+    # end2end test
+
+    def setUp(self):
+        self.spark_ai = SparkAI(
+            cache_file_location="examples/spark_ai_cache.json",
+            verbose=True,
+        )
+
+        # Generate heterogeneous JSON by randomly reorder list of keys and drop some of them
+        random_dict = {
+            "id": 1279,
+            "first_name": "John",
+            "last_name": "Doe",
+            "username": "johndoe",
+            "email": "john_doe@example.com",
+            "phone_number": "+1 234 567 8900",
+            "address": "123 Main St, Springfield, OH, 45503, USA",
+            "age": 32,
+            "registration_date": "2020-01-20T12:12:12Z",
+            "last_login": "2022-03-21T07:25:34Z",
+        }
+        self.json_keys = list(random_dict.keys())
+        rows = []
+        for _ in range(20):
+            keys = [k for k in random_dict.keys()]
+            shuffle(keys)
+            rows.append({k: random_dict[k] for k in keys if random() <= 0.6})
+
+        self.bad_json = self.spark.createDataFrame(
+            [(json.dumps(val), self.json_keys) for val in rows],
+            ["json_field", "schema"],
+        )
+        # Generate expected output of parsed JSON: list of fields in a right order or null if field is missing
+        self.expected_output = self.spark.createDataFrame(
+            [
+                (
+                    json.dumps(val),
+                    self.json_keys,
+                    [val.get(k, None) for k in self.json_keys],
+                )
+                for val in rows
+            ],
+            ["json_field", "schema", "parsed"],
+        )
+
+        # Add a DataFrame with random texts that contain emails
+        self.email_df = self.spark.createDataFrame(
+            [
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas nulla sit amet elit volutpat ultricies. Morbi lacinia est fringilla pulvinar elementum. Curabitur rhoncus luctus dui, sodales blandit arcu maximus a. Aenean iaculis nulla ac enim tincidunt, et tristique enim bibendum. Fusce mollis nibh sit amet nisi pellentesque egestas. Quisque volutpat, neque eu semper tristique, odio nunc auctor odio, at condimentum lorem nunc nec nisi. Quisque auctor at velit nec fermentum. Nunc id pellentesque erat, et dignissim felis. ali.brown@gmail.com Suspendisse potenti. Donec tincidunt enim in ipsum faucibus sollicitudin. Sed placerat tempor eros at blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec aliquam velit vehicula massa egestas faucibus. Ut pulvinar mi id pretium dignissim. Phasellus vehicula, dui sit amet porttitor effectively maximizes an attacker's chance to obtain valid credentials. Sed malesuada justo enim, et interdum mauris ullamcorper ac.",
+                "Vestibulum rhoncus magna semper est lobortis gravida. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In hac habitasse platea dictumst. michael.hobb@gmail.com Aenean sapien magna, consequat vitae pretium ac, gravida sit amet nibh. Maecenas lacinia orci in luctus placerat. Praesent lobortis turpis nec risus dapibus, eget ornare mi egestas. Nam eget dui ac mi laoreet sagittis. Integer condimentum est ac velit volutpat pharetra. Nulla facilisi. Nunc euismod, neque vitae porttitor maximus, justo dui efficitur ligula, vitae tincidunt erat neque ac nibh. Duis eu dui in erat blandit mattis.",
+                "Aenean vitae iaculis odio. Donec laoreet non urna sed posuere. Nulla vitae orci finibus, convallis mauris nec, mattis augue. Proin bibendum non justo non scelerisque. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean scott_p@ymail.com adipiscing diam eget ultrices ultricies. Aliquam bibendum dolor vel orci posuere, sed pulvinar enim rutrum. Nulla facilisi. Sed cursus justo sed velit pharetra auctor. Suspendisse facilisis nibh id nibh ultrices luctus.",
+                "Quisque varius erat sed leo ornare, et elementum leo interdum. Aliquam erat volutpat. Ut laoreet tempus elit quis venenatis. Integer porta, lorem ut pretium luctus, erika.23@hotmail.com quis ipsum facilisis, feugiat libero sed, malesuada augue. Fusce id elementum sapien, sed SC ingeniously maximizes the chance to obtain valid credentials. Nullam imperdiet felis in metus semper ultrices. Integer vel quam consectetur, lobortis est vitae, lobortis sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+                "Sed consectetur nisl quis mauris laoreet posuere. Phasellus in elementum orci, vitae auctor dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec eleifend mauris id auctor blandit. john.smith@protonmail.com Integer quis justo non eros convallis aliquet cursus eu dolor. Praesent nec sem a massa facilisis consectetur. Nunc pharetra sapien non erat semper, ut tempus risus vulputate. Donec lacinia condimentum arcu, ac molestie metus interdum in. Duis arcu quam, hendrerit quis venenatis sed, porta at erat.",
+            ],
+            schema="string",
+        )
+        self.parsed_email_df = self.spark.createDataFrame(
+            [
+                "ali.brown@gmail.com",
+                "michael.hobb@gmail.com",
+                "scott_p@ymail.com",
+                "erika.23@hotmail.com",
+                "john.smith@protonmail.com",
+            ],
+            schema="string",
+        )
+
+    def test_array_udf_output(self):
+        @self.spark_ai.udf
+        def parse_heterogeneous_json(json_str: str, schema: List[str]) -> List[str]:
+            """Extract fields from heterogeneous JSON string based on given schema in a right order. If field is missing replace it by None. All imports should be inside function."""
+            ...
+
+        self.spark.udf.register(
+            "parse_heterogeneous_json",
+            parse_heterogeneous_json,
+            returnType=ArrayType(elementType=StringType()),
+        )
+        res = self.bad_json.withColumn(
+            "parsed", expr("parse_heterogeneous_json(json_field, schema)")
+        )
+        assert_df_equality(res, self.expected_output)
+
+    def test_parse_email_udf(self):
+        @self.spark_ai.udf
+        def extract_email(text: str) -> str:
+            """Extract first email from raw text"""
+            ...
+
+        self.spark.udf.register("extract_email", extract_email)
+        assert_df_equality(
+            self.email_df.withColumn("value", expr("extract_email(value)")),
+            self.parsed_email_df,
+        )
+
+    def test_laplace_random_udf(self):
+        @self.spark_ai.udf
+        def laplace_random_number(loc: float, scale: float) -> float:
+            """Generate a random number from Laplace distribution with given loc and scale in pure Python. Function should contain all necessary imports."""
+            ...
+
+        numpy_results = np.random.laplace(1.0, 0.3, 500_000)
+        numpy_mean = numpy_results.mean()
+        numpy_stddev = numpy_results.std()
+
+        self.spark.udf.register(
+            "laplace_random_number", laplace_random_number, returnType=DoubleType()
+        )
+        spark_results = (
+            self.spark.sparkContext.range(0, 500_000)
+            .toDF(schema="int")
+            .withColumn("loc", lit(1.0).cast("double"))
+            .withColumn("scale", lit(0.3).cast("double"))
+            .withColumn("laplace_random", expr("laplace_random_number(loc, scale)"))
+            .select(
+                mean("laplace_random").alias("mean"),
+                stddev("laplace_random").alias("stddev"),
+            )
+            .collect()
+        )[0]
+
+        assert abs(spark_results["mean"] - numpy_mean) / numpy_mean <= 0.05
+        assert abs(spark_results["stddev"] - numpy_stddev) / numpy_stddev <= 0.05
 
 
 if __name__ == "__main__":
