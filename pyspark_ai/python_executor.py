pyspark-ai/pyspark-ai

commit 61e34f07fccf8b824950e24c1bda057a231756be

Implement Auto-Retry Mechanism for Python Code Generation (#159)
Introduce auto-retry framework for python code generation. The framework
can be used on:
* df.ai.plot
* df.ai.verify
* UDF generation.

![output](https://github.com/pyspark-ai/pyspark-ai/assets/1097932/1e2dc7ac-c90b-4827-9cab-6b6c88ef7dc1)


This PR starts with `df.ai.plot`. After changes, the plotting will be
like:
```
[CODE GEN 1]
INFO: Getting the following error: 
[UNRESOLVED_COLUMN.WITH_SUGGESTION] A column or function parameter with name `start_date` cannot be resolved. Did you mean one of the following? [`start_lat`, `start_lon`, `end_lat`, `end_lon`, `airline`, `airport1`, `airport2`, `cnt`].
INFO: Retrying with 3 retries left
INFO: I apologize for the confusion. It seems there was a misunderstanding in the requirements. If the DataFrame doesn't have a 'start_date' column, we can't filter for flights in February 2011. 
[CODE GEN 2]
...
```
Also, this PR:
1. improves the prompt of plotting
2. changes the return type of `df.ai.plot` from None to str, so that it
is easier for users to get the generated code, and easier to test
plotting.

pyspark_ai/python_executor.py
@@ -1,0 +1,91 @@
+from typing import Any, List, Optional
+
+from langchain import LLMChain
+from langchain.callbacks.manager import Callbacks
+from langchain.chat_models.base import BaseChatModel
+from langchain.schema import BaseMessage, HumanMessage
+from pyspark.sql import DataFrame
+
+from pyspark_ai.code_logger import CodeLogger
+
+from pyspark_ai.ai_utils import AIUtils
+
+from pyspark_ai.cache import Cache
+from pyspark_ai.temp_view_utils import canonize_string
+
+SKIP_CACHE_TAGS = ["SKIP_CACHE"]
+
+
+class PythonExecutor(LLMChain):
+    """LLM Chain to generate python code. It supports caching and retrying."""
+
+    df: DataFrame
+    cache: Cache = None
+    logger: CodeLogger
+    max_retries: int = 3
+
+    def run(
+        self,
+        *args: Any,
+        callbacks: Callbacks = None,
+        tags: Optional[List[str]] = None,
+        **kwargs: Any,
+    ) -> str:
+        assert not args, "The chain expected no arguments"
+        # assert llm is an instance of BaseChatModel
+        assert isinstance(
+            self.llm, BaseChatModel
+        ), "The llm is not an instance of BaseChatModel"
+        prompt_str = canonize_string(self.prompt.format_prompt(**kwargs).to_string())
+        use_cache = tags != SKIP_CACHE_TAGS
+        if self.cache is not None:
+            cached_result = self.cache.lookup(prompt_str) if use_cache else None
+            if cached_result is not None:
+                self._execute_code(self.df, cached_result)
+                return cached_result
+        messages = [HumanMessage(content=prompt_str)]
+        response = self._generate_python_with_retries(
+            self.df, self.llm, messages, self.max_retries
+        )
+        if use_cache and self.cache is not None:
+            self.cache.update(prompt_str, response)
+        return response
+
+    @staticmethod
+    def _execute_code(df: DataFrame, code: str):
+        import pandas as pd  # noqa: F401
+
+        exec(compile(code, "plot_df-CodeGen", "exec"))
+
+    def _generate_python_with_retries(
+        self,
+        df: DataFrame,
+        chat_model: BaseChatModel,
+        messages: List[BaseMessage],
+        retries: int = 3,
+    ) -> str:
+        response = chat_model.predict_messages(messages)
+        if self.logger is not None:
+            self.logger.info(response.content)
+        code = "\n".join(AIUtils.extract_code_blocks(response.content))
+        try:
+            self._execute_code(df, code)
+            return code
+        except Exception as e:
+            if self.logger is not None:
+                self.logger.warning("Getting the following error: \n" + str(e))
+            if retries <= 0:
+                # if we have no more retries, raise the exception
+                self.logger.info(
+                    "No more retries left, please modify the instruction or modify the generated code"
+                )
+                return ""
+            if self.logger is not None:
+                self.logger.info("Retrying with " + str(retries) + " retries left")
+
+            messages.append(response)
+            # append the exception as a HumanMessage into messages
+            messages.append(HumanMessage(content=str(e)))
+            return self._generate_python_with_retries(
+                df, chat_model, messages, retries - 1
+            )

tests/test_end_to_end.py
@@ -212,3 +212,10 @@ class EndToEndTestCase(unittest.TestCase):
 
         assert abs(spark_results["mean"] - numpy_mean) / numpy_mean <= 0.05
         assert abs(spark_results["stddev"] - numpy_stddev) / numpy_stddev <= 0.05
+
+    def test_plot(self):
+        flight_df = self.spark_ai._spark.read.option("header", "true").csv("data/2011_february_aa_flight_paths.csv")
+        # The following plotting will probably fail on the first run with error:
+        #     'DataFrame' object has no attribute 'date'
+        code = flight_df.ai.plot("Boxplot summarizing the range of starting latitudes for all AA flights in February 2011.")
+        assert(code != "")

pyspark_ai/code_logger.py
@@ -7,13 +7,18 @@ from pygments.formatters import TerminalFormatter
 from pygments.lexers import PythonLexer, SqlLexer
 
 GREEN = "\033[92m"  # terminal code for green
+YELLOW = "\033[93m"  # terminal code for yellow
 RESET = "\033[0m"  # reset terminal color
 
 
 # Custom Formatter
 class CustomFormatter(logging.Formatter):
     def format(self, record):
-        return GREEN + "INFO: " + RESET + super().format(record)
+        if record.levelname == "INFO":
+            return GREEN + "INFO: " + RESET + super().format(record)
+        elif record.levelname == "WARNING":
+            return YELLOW + "WARN: " + RESET + super().format(record)
+        return super().format(record)
 
 
 class CodeLogger:
@@ -37,7 +42,7 @@ class CodeLogger:
             raise ValueError(f"Unsupported language: {language}")
         return highlight(code, lexer, TerminalFormatter())
 
-    def log(self, message):
+    def info(self, message):
         # Define pattern to match code blocks with optional language specifiers
         pattern = r"```(python|sql)?(.*?)```"
         # Split message into parts. Every 3rd part will be a code block.
@@ -54,3 +59,6 @@ class CodeLogger:
                 )
         # Log the message with colored code blocks
         self.logger.info(colored_message)
+
+    def warning(self, message):
+        self.logger.warning(message)

pyspark_ai/llm_chain_with_cache.py
@@ -12,14 +12,6 @@ SKIP_CACHE_TAGS = ["SKIP_CACHE"]
 class LLMChainWithCache(LLMChain):
     cache: Cache
 
-    @staticmethod
-    def _sort_and_stringify(*args: Any) -> str:
-        # Convert all arguments to strings, then sort them
-        sorted_args = sorted(str(arg) for arg in args)
-        # Join all the sorted, stringified arguments with a space
-        result = " ".join(sorted_args)
-        return result
-
     def run(
         self,
         *args: Any,

pyspark_ai/prompt.py
@@ -227,31 +227,25 @@ EXPLAIN_DF_PROMPT = FewShotPromptTemplate(
 )
 
 PLOT_PROMPT_TEMPLATE = """
-You are an Apache Spark SQL expert programmer.
-It is forbidden to include old deprecated APIs in your code.
-For example, you will not use the pandas method "append" because it is deprecated.
-
 Given a pyspark DataFrame `df`, with the output columns:
 {columns}
 
-And an explanation of `df`: {explain}
+Write Python code to visualize the result of `df` using plotly:
+1. Do any aggregation against `df` first, before converting the `df` to a pandas DataFrame. 
+2. Make sure to use the exact column names of `df`.
+3. Your code may NOT contain "append" anywhere. Instead of append, use pd.concat.
+4. There is no need to install any package with pip. Do include any necessary import statements.
+5. Display the plot directly, instead of saving into an HTML.
+6. Do not use scatter plot to display any kind of percentage data.
+7. You must import and start your Spark session with `spark = SparkSession.builder.getOrCreate()`.
+8. It is forbidden to include old deprecated APIs in your code.
+9. Ensure that your code is correct.
 
-Write Python code to visualize the result of `df` using plotly. Do any aggregation against `df` 
-first, before converting the `df` to a pandas DataFrame. Make sure to use the exact column names 
-of `df`.
-Your code may NOT contain "append" anywhere. Instead of append, use pd.concat.
-There is no need to install any package with pip. Do include any necessary import statements.
-Display the plot directly, instead of saving into an HTML.
-Do not use scatter plot to display any kind of percentage data.
-You must import and start your Spark session if you use a Spark DataFrame.
-Remember to ensure that your code does NOT include "append" anywhere, under any circumstance (use pd.concat instead).
-
-Ensure that your code is correct.
 {instruction}
 """
 
 PLOT_PROMPT = PromptTemplate(
-    input_variables=["columns", "explain", "instruction"], template=PLOT_PROMPT_TEMPLATE
+    input_variables=["columns", "instruction"], template=PLOT_PROMPT_TEMPLATE
 )
 
 VERIFY_TEMPLATE = """

pyspark_ai/pyspark_ai.py
@@ -5,7 +5,6 @@ import re
 from typing import Callable, List, Optional
 from urllib.parse import urlparse
 
-import pandas as pd  # noqa: F401
 import requests
 import tiktoken
 from bs4 import BeautifulSoup
@@ -28,6 +27,7 @@ from pyspark_ai.prompt import (
     UDF_PROMPT,
     VERIFY_PROMPT,
 )
+from pyspark_ai.python_executor import PythonExecutor
 from pyspark_ai.react_spark_sql_agent import ReActSparkSQLAgent
 from pyspark_ai.search_tool_with_cache import SearchToolWithCache
 from pyspark_ai.temp_view_utils import (
@@ -119,13 +119,14 @@ class SparkAI:
         self._sql_llm_chain = self._create_llm_chain(prompt=SQL_PROMPT)
         self._explain_chain = self._create_llm_chain(prompt=EXPLAIN_DF_PROMPT)
         self._sql_agent = self._create_sql_agent()
-        self._plot_chain = self._create_llm_chain(prompt=PLOT_PROMPT)
         self._verify_chain = self._create_llm_chain(prompt=VERIFY_PROMPT)
         self._udf_chain = self._create_llm_chain(prompt=UDF_PROMPT)
         self._sample_rows_in_table_info = sample_rows_in_table_info
         self._verbose = verbose
         if verbose:
             self._logger = CodeLogger("spark_ai")
+        else:
+            self._logger = None
 
     def _create_llm_chain(self, prompt: BasePromptTemplate):
         if self._cache is None:
@@ -206,7 +207,7 @@ class SparkAI:
 
     def log(self, message: str) -> None:
         if self._verbose:
-            self._logger.log(message)
+            self._logger.info(message)
 
     def _trim_text_from_end(self, text: str, max_tokens: int) -> str:
         """
@@ -265,7 +266,8 @@ class SparkAI:
         self._spark.sql(sql_query)
         return self._spark.table(view_name)
 
-    def _get_df_schema(self, df: DataFrame) -> list:
+    @staticmethod
+    def _get_df_schema(df: DataFrame) -> list:
         schema_lst = [f"{name}: {dtype}" for name, dtype in df.dtypes]
         return schema_lst
 
@@ -505,22 +507,33 @@ class SparkAI:
 
     def plot_df(
         self, df: DataFrame, desc: Optional[str] = None, cache: bool = True
-    ) -> None:
+    ) -> str:
+        """
+        Plot a Spark DataFrame, the specifics of which are determined by the `desc` parameter.
+        If `desc` is not provided, the method will try to plot the DataFrame based on its schema.
+
+        :param df: The PySpark dataframe to generate plotting code for.
+        :param desc: An optional natural language string that outlines the specific transformation to be applied on the
+                     DataFrame.
+        :param cache: Whether to cache the dataframe or not. Default is True.
+
+        :return: Returns the generated code as a string. If the generated code is not valid Python code, an empty string
+                 is returned.
+        """
         instruction = f"The purpose of the plot: {desc}" if desc is not None else ""
         tags = self._get_tags(cache)
-        response = self._plot_chain.run(
+        plot_chain = PythonExecutor(
+            df=df,
+            prompt=PLOT_PROMPT,
+            cache=self._cache,
+            llm=self._llm,
+            logger=self._logger,
+        )
+        return plot_chain.run(
             tags=tags,
             columns=self._get_df_schema(df),
-            explain=self._get_df_explain(df, cache),
             instruction=instruction,
         )
-        self.log(response)
-        codeblocks = AIUtils.extract_code_blocks(response)
-        code = "\n".join(codeblocks)
-        try:
-            exec(compile(code, "plot_df-CodeGen", "exec"))
-        except Exception as e:
-            raise Exception("Could not evaluate Python code", e)
 
     def verify_df(self, df: DataFrame, desc: str, cache: bool = True) -> bool:
         """

tests/test_python_executor.py
@@ -1,0 +1,85 @@
+import unittest
+from typing import List, Optional, Any
+
+from langchain.callbacks.manager import CallbackManagerForLLMRun, AsyncCallbackManagerForLLMRun
+from langchain.chat_models.base import BaseChatModel
+from langchain.schema import BaseMessage, AIMessage, ChatResult
+from pyspark.sql import SparkSession
+
+from pyspark_ai import SparkAI
+from pyspark_ai.prompt import PLOT_PROMPT
+
+from pyspark_ai.code_logger import CodeLogger
+
+from pyspark_ai.python_executor import PythonExecutor
+
+
+# Test case for PythonExecutor.
+# Mock the following:
+#   - llm
+# For the first call of llm.predict_messages, return a response with content of "1 1", so that it will fail.
+# Verify that PythonExecutor will retry and succeed.
+# For the second call of llm.predict_messages, return a response with content of "1 + 1"
+# Verify that PythonExecutor will succeed.
+class TestPythonExecutor(unittest.TestCase):
+    # Mock llm
+    class MockLLM(BaseChatModel):
+        predict_messages_calls: int
+
+        def _generate(
+                self,
+                messages: List[BaseMessage],
+                stop: Optional[List[str]] = None,
+                run_manager: Optional[CallbackManagerForLLMRun] = None,
+                **kwargs: Any) -> ChatResult:
+            pass
+
+        async def _agenerate(
+                self, messages: List[BaseMessage],
+                stop: Optional[List[str]] = None,
+                run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
+                **kwargs: Any) -> ChatResult:
+            pass
+
+        @property
+        def _llm_type(self) -> str:
+            pass
+
+        def __init__(self):
+            super().__init__(predict_messages_calls=0)
+            self.predict_messages_calls = 0
+
+        def predict_messages(self, messages):
+            self.predict_messages_calls += 1
+            if self.predict_messages_calls == 1:
+                return AIMessage(content="1 1")
+            return AIMessage(content="1 + 1")
+
+    def test_retry(self):
+        df = SparkSession.builder.getOrCreate().createDataFrame([("1", "1")], ["input", "response"])
+        llm = self.MockLLM()
+        executor = PythonExecutor(
+            llm=llm,
+            df=df,
+            prompt=PLOT_PROMPT,
+            logger=CodeLogger("test"))
+        executor.run(
+            columns=SparkAI._get_df_schema(df),
+            instruction="plot",
+        )
+        self.assertEqual(llm.predict_messages_calls, 2)
+
+    def test_no_retry(self):
+        df = SparkSession.builder.getOrCreate().createDataFrame([("1", "1")], ["input", "response"])
+        llm = self.MockLLM()
+        executor = PythonExecutor(
+            llm=llm,
+            df=df,
+            prompt=PLOT_PROMPT,
+            logger=CodeLogger("test"),
+            max_retries=0)
+        executor.run(
+            columns=SparkAI._get_df_schema(df),
+            instruction="plot",
+        )
+        self.assertEqual(llm.predict_messages_calls, 1)

commit 0c9e4cf0ad944edb466b5cffc7291663e55a95df

Fix regression on the plotting API when verbose is False (#161)
Fix the following regression from
https://github.com/pyspark-ai/pyspark-ai/pull/159:
```
>>> df.ai.plot("Bar plot of the top 5 most populous cities")
Traceback (most recent call last):
  File "<stdin>", line 1, in <module>
  File "/Users/xinrong.meng/pyspark-ai/pyspark_ai/ai_utils.py", line 66, in plot
    return self.spark_ai.plot_df(self.df_instance, desc, cache)
  File "/Users/xinrong.meng/pyspark-ai/pyspark_ai/pyspark_ai.py", line 525, in plot_df
    plot_chain = PythonExecutor(
  File "/opt/miniconda3/envs/eng_dev/lib/python3.9/site-packages/langchain/load/serializable.py", line 74, in __init__
    super().__init__(**kwargs)
  File "pydantic/main.py", line 341, in pydantic.main.BaseModel.__init__
pydantic.error_wrappers.ValidationError: 1 validation error for PythonExecutor
logger
  none is not an allowed value (type=type_error.none.not_allowed)
```

Also, this PR fixes the file location in the end_to_end test case

pyspark_ai/python_executor.py
@@ -21,7 +21,7 @@ class PythonExecutor(LLMChain):
 
     df: DataFrame
     cache: Cache = None
-    logger: CodeLogger
+    logger: CodeLogger = None
     max_retries: int = 3
 
     def run(

tests/test_end_to_end.py
@@ -214,7 +214,7 @@ class EndToEndTestCase(unittest.TestCase):
         assert abs(spark_results["stddev"] - numpy_stddev) / numpy_stddev <= 0.05
 
     def test_plot(self):
-        flight_df = self.spark_ai._spark.read.option("header", "true").csv("data/2011_february_aa_flight_paths.csv")
+        flight_df = self.spark_ai._spark.read.option("header", "true").csv("tests/data/2011_february_aa_flight_paths.csv")
         # The following plotting will probably fail on the first run with error:
         #     'DataFrame' object has no attribute 'date'
         code = flight_df.ai.plot("Boxplot summarizing the range of starting latitudes for all AA flights in February 2011.")